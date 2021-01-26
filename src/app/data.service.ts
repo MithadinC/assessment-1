d512efa7ef7441409fd28147b0efe9b05196706b
Mithadin <mithadin.btech@outlook.com>
2021-01-26 23:01:40 +0530

    Sorting tsble on load functionality

--- src/app/data.service.ts
+++ src/app/data.service.ts
@@ -9,12 +9,28 @@ export class DataService {
   constructor() { }
 
   getData(): any[] {
-    return employees;
+    return this.getDataSorted("nameasc");;
   }
 
   getDepartments() {
     const departments = Array.from(new Set(employees.map(el => el.departments).flat()));
-    return (departments)
+    return departments;
+  }
+
+  getDataSorted(sortType: string): any[] {
+    if (sortType === "nameasc") {
+      return employees.sort((a, b) => {
+        let fa = a.name.toLowerCase(),
+          fb = b.name.toLowerCase();
+        if (fa < fb) {
+          return -1;
+        }
+        if (fa > fb) {
+          return 1;
+        }
+        return 0;
+      });
+    }
   }
 
 }
